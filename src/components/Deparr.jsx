0c08aae3308bf2b859f36b3e551308ec32600c07
Misposition <itsnotpublicc@gmail.com>
2022-06-08 18:24:16 -0400

    Setup table to structure dep/arr data.

--- src/components/Deparr.jsx
+++ src/components/Deparr.jsx
@@ -1,8 +1,6 @@
 import {useState, useEffect} from 'react'
 import {r7, r8, r9} from './Regex'
 import Schedule from './Schedule'
-
-
 function Deparr(props) {
 	const [departures, setDepartures] = useState()
 	const [arrivals, setArrivals] = useState()
@@ -23,6 +21,9 @@ function Deparr(props) {
 					setAirport(data)
 					timeFixDep(data1)
 					timeFixArr(data2)
+					if(!data1.error | !data2.error){
+					console.log('success')
+					}
 				} else alert('There was no record found of the queried airport.')
 			}
 		}
@@ -36,6 +37,7 @@ function Deparr(props) {
 			let day = date.slice(8,10)
 			let time = depTime.slice(11,16)
 			dep[i].departure.scheduledTime = `${day}/${month}/${year} ${time}`
+			dep[i].departure.scheduledTime = `${day}/${month}/${year} ${time}`
 		}
 		setDepartures(dep)
 	}
@@ -48,34 +50,14 @@ function Deparr(props) {
 			let day = date.slice(8,10)
 			let time = depTime.slice(11,16)
 			arr[i].arrival.scheduledTime = `${day}/${month}/${year} ${time}`
+			arr[i].arrival.scheduledTime = `${day}/${month}/${year} ${time}`
 		}
 		setArrivals(arr)
 	}
 	useEffect(() => {
 		load();
 	}, [search])
-	return (arrivals ? <Schedule arrivals={arrivals} departures={departures} airport={airport} /> : null)
+	return (arrivals ? <Schedule arrivals={arrivals} departures={departures} airport={airport} /> : <div>Not found.</div>)
 }
 
-// const scheduled = data.map((info, i) => {
-	// 	const schedule = info.filter()
-	// })
-	// let i
-	
-	// const scheduled = data.filter(schedule => schedule.status === 'scheduled')
-	// const sortbyTerm = function(a,b){
-	// 	if(a.departure.terminal > b.departure.terminal){
-	// 		return 1;
-	// 	}
-	// 	if(a.departure.terminal < b.departure.terminal){
-	// 		return -1;
-	// 	}
-	// 	return a.departure.terminal - b.departure.terminal
-	// }
-	// const terminal = scheduled.sort(sortbyTerm)
-	// for(i=0; i < terminal.length; i++){
-	// 	if(terminal[i].departure.terminal === '1'){
-			
-	// 	}
-	// }
 export default Deparr;
--- src/components/Regex.jsx
+++ src/components/Regex.jsx
@@ -7,10 +7,4 @@ const r6 = new RegExp(/^[A-Z]{3}[0-9]{4}/);
 const r7 = new RegExp(/^[A-Z]{3}/);
 const r8 = new RegExp(/^[0-9]{1}/);
 
-
-// function isValidFln(FLN){
-// 	if(r1.test(FLN) | r2.test(FLN) | r3.test)
-
-// }
-
 module.exports = {r1, r2, r3, r4, r5, r6, r7, r8};
