6198b71a3b21b63438caf05941727f1abacbb145
Misposition <itsnotpublicc@gmail.com>
2022-06-10 19:27:14 -0400

    Fixed Registration routes, and registration form fully functional.

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -8,7 +8,7 @@ function Register() {
 	
 	const registerUser = (async (e) => {
 		e.preventDefault()
-		const response = await fetch('http://localhost:5000/api/register', {
+		const response = await fetch('http://localhost:5000/register', {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json',
